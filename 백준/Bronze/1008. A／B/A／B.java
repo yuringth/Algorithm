66ac91f6f76affb7bad7693c7930cd1f5ba1e18e
yuringth <121650385+yuringth@users.noreply.github.com>
2023-04-08 17:32:40 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args){
+    
+        Scanner sc = new Scanner(System.in);
+        
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+        
+        System.out.println(A/B);
+        
+        sc.close();
+        
+    }
+    
+}
